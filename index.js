204a261915f670d40752e88ffc07790886c0f921
27px <twentysevenpixels.com>
2022-01-26 12:13:06 +0530

    added results in single line with clear console

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
 const prompts = require("prompts");
 const chalk = require("chalk");
 const wordsJSON = require("./words.json");
+const terminal_cols = process.stdout.columns;
+const MAX_TRIES = 6;
 
+// global results for showing all results at once
+let glob_results = "";
 let puzzle = "";
 
 const wordlePrompt = {
   type: "text",
   name: "word",
-  message: "Enter a 5 word...",
-  validate: value => value.length != 5 ? 'Word must be 5 letters' : true
+  message: "Enter a 5 letter word...",
+  validate: (value) => {
+    if (value.length != 5) {
+      return "Word must be 5 letters";
+    } else if (!/^[a-z]+$/i.test(value)) {
+      return "Word must only contain alphabets";
+    }
+    return true;
+  },
 };
 
 async function check(guess) {
-  let results = [];
+  // clear previous results
+  console.clear();
+  let results = "";
   // loop over each letter in the word
   for (let i in guess) {
-    let attempt = { letter: guess[i], color: "bgGrey" };
     // check if the letter at the specified index in the guess word exactly
     // matches the letter at the specified index in the puzzle
-    if (attempt.letter === puzzle[i]) {
-      process.stdout.write(chalk.white.bgGreen.bold(` ${guess[i]} \t`));
+    if (guess[i] === puzzle[i]) {
+      results += chalk.black.bgGreen.bold(` ${guess[i]} `) + " ";
       continue;
     }
     // check if the letter at the specified index in the guess word is at least
     // contained in the puzzle at some other position
-    if (puzzle.includes(attempt.letter)) {
-      process.stdout.write(chalk.white.bgYellow.bold(` ${guess[i]} \t`));
+    if (puzzle.includes(guess[i])) {
+      results += chalk.black.bgYellow.bold(` ${guess[i]} `) + " ";
       continue;
     }
     // otherwise the letter doesn't exist at all in the puzzle
-    process.stdout.write(chalk.white.bgGrey.bold(` ${guess[i]} \t`));
+    results += chalk.white.bgGrey.bold(` ${guess[i]} `) + " ";
   }
-  return results;
+  // 15 in below code is 5 letters and 2 spaces in start and end of characters, 3 char for a letter, total 3 *5 =15
+  glob_results += results.padEnd(results.length + terminal_cols - 15, " ");
+  process.stdout.write(glob_results);
 }
 
 async function play(tries) {
   // the user gets 5 tries to solve the puzzle not including the first guess
-  if (tries < 6) {
+  if (tries < MAX_TRIES) {
     // ask the player for a guess word
     const response = await prompts(wordlePrompt);
     const guess = response.word.toUpperCase();
     // if the word matches, they win!
     if (guess == puzzle) {
+      // show board again
+      check(guess);
       console.log("WINNER!");
     } else {
       check(guess);
@@ -59,7 +75,6 @@ async function main() {
   // get a random word
   randomNumber = Math.floor(Math.random(wordsJSON.length) * wordsJSON.length);
   puzzle = wordsJSON[randomNumber].toUpperCase();
-
   // start the game
   await play(0);
 }

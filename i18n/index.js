cfe8b65ea5685656a5ac26744a2dd0de395d197c
Javier García Álvarez <javigaralva@gmail.com>
2022-02-02 20:40:22 +0100

    style: use the same Burke's code format

--- i18n/index.js
+++ i18n/index.js
@@ -1,15 +1,15 @@
-function load( lang = "en" ) {
+function load(lang = "en") {
   try {
     return {
-      WORDS: require( `./words-${lang}.json` ),
-      TEXTS: require( `./texts-${lang}.json` )
+      WORDS: require(`./words-${lang}.json`),
+      TEXTS: require(`./texts-${lang}.json`)
     }
   } catch(e) {
     return load("en");
   }
 }
 
-function stringTemplateParser( expression, valueObj ) {
+function stringTemplateParser(expression, valueObj) {
   const templateMatcher = /{{\s?([^{}\s]*)\s?}}/g;
   let text = expression.replace(templateMatcher, (substring, value, index) => {
     value = valueObj[value];
